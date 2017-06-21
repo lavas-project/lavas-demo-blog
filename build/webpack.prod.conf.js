28335b306772d5db06c9651d1b0a00167e892bc1
tanglei02 <tanglei02@baidu.com>
2017-06-21 18:46:10 +0800

    用回原来的plugin

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -18,9 +18,9 @@ var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin');
 var SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
 var SkeletonWebpackPlugin = require('vue-skeleton-webpack-plugin');
 var SwRegisterWebpackPlugin = require('sw-register-webpack-plugin');
-// var MultiPathWebpackPlugin = require('multi-path-webpack-plugin');
+var MultiPathWebpackPlugin = require('multi-path-webpack-plugin');
 var APIWebpackPlugin = require('./plugins/api-webpack-plugin');
-var PublicPathWebpackPlugin = require('./plugins/publicpath-webpack-plugin');
+// var PublicPathWebpackPlugin = require('./plugins/publicpath-webpack-plugin');
 
 var env = process.env.NODE_ENV === 'testing'
     ? require('../config/test.env')
@@ -127,14 +127,14 @@ var webpackConfig = merge(baseWebpackConfig, {
         // service worker caching
         new SWPrecacheWebpackPlugin(config.swPrecache.build),
         new SwRegisterWebpackPlugin({}),
-        // new MultiPathWebpackPlugin({
-        //     prefix: config.build.assetsPublicPath
-        // }),
+        new MultiPathWebpackPlugin({
+            prefix: config.build.assetsPublicPath
+        }),
         new APIWebpackPlugin(),
-        new PublicPathWebpackPlugin({
-            publicPath: config.build.assetsPublicPath,
-            static: config.build.assetsSubDirectory
-        })
+        // new PublicPathWebpackPlugin({
+        //     publicPath: config.build.assetsPublicPath,
+        //     static: config.build.assetsSubDirectory
+        // })
     ]
 });
 
