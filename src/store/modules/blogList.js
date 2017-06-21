9610d675c1dc12b7d5b7580055a4d82a585316cc
chenqiushi <tracy909@126.com>
2017-06-21 12:16:55 +0800

    replace tabs with spaces

--- src/store/modules/blogList.js
+++ src/store/modules/blogList.js
@@ -7,34 +7,34 @@ import * as types from '../mutation-custom-types';
 import API from '@/api';
 
 export default {
-	state: {
-		blogList: [],
-		loadingStatus: false,
-		blogDetail: {}
-	},
-	getters: {
-		blogList: state => state.blogList,
-		loadingStatus: state => state.loadingStatus,
-		blogDetail: state => state.blogDetail
-	},
-	actions: {
-		async getBlogList({commit}, params) {
-			try {
-				let {blogs} = await API.getBlogList(params);
-				commit(types.SET_BLOG_LIST, {blogs});
-			}
-			catch (e) {}
-		},
-		async getBlogDetail({commit}, params) {
-			try {
-				let {blogDetail} = await API.getBlogDetail(params);
-				commit(types.SET_BLOG_DETAIL, {blogDetail});
-			}
-			catch (e) {}
-		}
-	},
-	mutations: {
-		[types.SET_BLOG_LIST](state, {blogs}) {
+    state: {
+        blogList: [],
+        loadingStatus: false,
+        blogDetail: {}
+    },
+    getters: {
+        blogList: state => state.blogList,
+        loadingStatus: state => state.loadingStatus,
+        blogDetail: state => state.blogDetail
+    },
+    actions: {
+        async getBlogList({commit}, params) {
+            try {
+                let {blogs} = await API.getBlogList(params);
+                commit(types.SET_BLOG_LIST, {blogs});
+            }
+            catch (e) {}
+        },
+        async getBlogDetail({commit}, params) {
+            try {
+                let {blogDetail} = await API.getBlogDetail(params);
+                commit(types.SET_BLOG_DETAIL, {blogDetail});
+            }
+            catch (e) {}
+        }
+    },
+    mutations: {
+        [types.SET_BLOG_LIST](state, {blogs}) {
             blogs.map(item => {
                 let time = new Date(Number(item.ts) || Date.now());
                 item.time = time.getFullYear() + '-' + time.getMonth() + '-'
@@ -43,20 +43,20 @@ export default {
             });
 
             if (blogs.length) {
-				state.blogList = state.blogList.concat(blogs);
-				state.loadingStatus = 'loaded';
+                state.blogList = state.blogList.concat(blogs);
+                state.loadingStatus = 'loaded';
             }
             else {
-            	state.loadingStatus = 'complete';
+                state.loadingStatus = 'complete';
             }
-		},
-		[types.SET_BLOG_DETAIL](state, {blogDetail}) {
+        },
+        [types.SET_BLOG_DETAIL](state, {blogDetail}) {
             let time = new Date(Number(blogDetail.ts) || Date.now());
             blogDetail.time = time.getFullYear() + '-' + time.getMonth() + '-'
                 + time.getDay() + ' ' + time.getHours() + ':'
                 + time.getMinutes();
 
-			state.blogDetail = blogDetail;
-		}
-	}
+            state.blogDetail = blogDetail;
+        }
+    }
 }
